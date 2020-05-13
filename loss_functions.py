552dfe5f9f7c3ffb5914e86c940406b5791cce6b
Shivanshu17 <sharma.shivanshu98@gmail.com>
2020-05-13 11:35:46 +0530

    Added quanitle and logcosh loss

--- loss_functions.py
+++ loss_functions.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import math
 def mean_squared_loss(y, pred_y):
     assert len(y) == len(pred_y)
     return (reduce((lambda item1, item2: item1 + item2), map(lambda x: x**2, (y-pred_y)))/len(pred_y))
@@ -15,4 +16,22 @@ def huber_loss(y, pred_y, t):
     else:
         return( t*reduce((lambda item1, item2: item1 + item2), map(abs, (y-pred_y)))/len(pred_y) - (t**2)/2)
     
+def log_cosh_loss(y, pred_y):
+    assert len(y) == len(pred_y)
+    log_cosh = lambda x: math.log(math.cosh(x))
+    return (reduce((lambda item1, item2: item1 + item2), map(log_cosh, map(abs, y-pred_y))))
 
+def quantile_loss(y, pred_y, q):
+    assert len(y) == len(pred_y)
+    assert q<=1 and q>=0
+    loss = 0
+    for i in range(len(y)):
+        if (y[i]-pred_y[i]>0):
+            loss = loss + q*(y[i]-pred_y[i])
+        else:
+            loss = loss + (q-1)*(y[i] - pred_y[i])
+    
+    return (loss/len(y))
+
+
+    
