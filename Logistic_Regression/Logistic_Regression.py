dfef2909d3b19c4d5ba863f5bede36d5d7f55257
Shivanshu17 <sharma.shivanshu98@gmail.com>
2020-07-02 08:06:54 +0530

    Added the logistic regression file

---
+++ Logistic_Regression/Logistic_Regression.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+import sys
+sys.path.append('/home/epoch/Shivanshu/Git-Projects/ML-From-Scratch/optim')
+# The following files will be imported from the path appended above
+import loss_functions as lf
+import sgd
+import Adagrad
+import Adadelta
+import Adam
+
